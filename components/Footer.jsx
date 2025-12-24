366db47f8e0c763894f362d7a386b86293a785a3
Remon <remongamal282@gmail.com>
2025-12-24 21:09:43 +0200

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small className="copy">
+        © 2025 Remon Gamal development. All rights reserved.
+      </small>
+    </footer>
+  );
+}
